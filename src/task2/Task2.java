fcdd9533958020fed28eac145568b9576917be1c
Mna <mennaallahyasser003@gmail.com>
2023-02-27 21:35:18 +0200

    solid added

--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -8,27 +8,7 @@ import Model.*;
 public class Task2 {
     
     public static void main(String[] args) {
-        Teacher t1=new Teacher();
-        
-        Course c1=new Course();
-        c1.setName("math");
-        c1.setCode("123");
-        c1.setTeacher(t1);
-        Course c2=new Course();
-        c1.setName("math2");
-        c1.setCode("123");
-        
-        Student s1=new Student();
-        s1.setId("1");
-        s1.setName("mna");
-        //s1.setCourseToStudent(c1);
-        System.out.println(c1.toString());
-        //System.out.println();
-        /*t1.setId(3);
-        t1.setName("mnsor");
-        t1.setTeacherCourse(c1);
-        t1.setTeacherCourse(c2);
-        System.out.println(t1.toString());*/
+       
         
     }
     
